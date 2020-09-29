a0e78b772f826c355c197c103f3692e0eb0d0a51
yangdapeng <yangdapeng@yurun.com>
2020-09-29 17:33:23 +0800

    feat :sparkles: 添加全局样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Button from './components/Button'
+
+
 const App: React.FC = () => {
     return (
         <>
             <div className="App">
                 app
+                <Button onClick={()=>{
+                    console.log('ddd')
+                }}>ddd</Button>
             </div>
         </>
     )
