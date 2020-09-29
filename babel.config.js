95919493a9d92028097fe9b5a0de807d9de67303
yangdapeng <yangdapeng@yurun.com>
2020-09-29 10:13:06 +0800

    chore: 修改typescript版本到4.0，全面支持es2020

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports={
-    presets: ['react'],
-    'plugins':[
-        '@babel/plugin-proposal-optional-chaining',
-        '@babel/plugin-proposal-nullish-coalescing-operator'
-    ]
-}
