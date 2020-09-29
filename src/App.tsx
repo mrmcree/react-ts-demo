154ebd8ec4352c1649a67a7cb6fd14b5976dcb6a
yangdapeng <yangdapeng@yurun.com>
2020-09-29 10:43:06 +0800

    feat: 使用了useContext

--- src/App.tsx
+++ src/App.tsx
@@ -9,18 +9,36 @@ interface ShowResult {
     status: number
 }
 
+interface ThemeProps {
+    [key: string]: { color: string, background: string }
+}
+
+ const themes: ThemeProps = {
+    'light': {
+        color: '#000',
+        background: "#fff"
+    },
+    'dark': {
+        color: "#fff",
+        background: '#000'
+    }
+}
+
+export const ThemeContext = React.createContext(themes.light)
 const App: React.FC = () => {
     const [data, loading] = useURLLoader('https://dog.ceo/api/breeds/image/random')
     const dogResult = data as ShowResult
     return (
         <>
             <div className="App">
+                <ThemeContext.Provider value={themes.light}>
                 <header className="App-header">
                     <Hello msg='hello'/>
                     <LinkButton/>
-                    {loading ? <p>读取中</p> : <img alt='dog' src={dogResult?.message}/>}
+                    {loading ? <p>读取中</p> : <img alt='dog' style={{width:'10px'}} src={dogResult?.message}/>}
 
                 </header>
+                </ThemeContext.Provider>
             </div>
         </>
     )
--- src/components/LinkButon.tsx
+++ src/components/LinkButon.tsx
@@ -1,14 +1,28 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef, useContext} from 'react'
+import {ThemeContext} from '../App'
 
 const LinkButton: React.FC = () => {
     const [like, setLike] = useState(0)
+    const theme = useContext(ThemeContext)
+
     /* useEffect dom加载完执行 */
     useEffect(() => {
         document.title = `点击了${like}次`
+        if (domRef && domRef.current)
+            domRef?.current.focus()
+    })
+    const domRef = useRef<HTMLInputElement>(null)
+    const handleSwitch=(()=>{
+        console.log(ThemeContext)
+
     })
     return (
         <>
+            <input ref={domRef}/>
+            <button onClick={handleSwitch}>switch theme</button>
+            <p  style={theme}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur, impedit.</p>
             <button
+
                 onClick={() => {
                     setLike(like + 1)
                 }}>
