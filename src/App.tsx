095107d26467de272eeb91c1c7cb766c5e93e2a1
yangdapeng <yangdapeng@yurun.com>
2020-09-29 09:29:16 +0800

    feat 添加一个requestHook

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import * as Components from './components'
+import useURLLoader from "./hooks/useURLLoader";
+
 const {Hello, LinkButton} = Components
-/*app*/
+
+interface ShowResult {
+    message: string,
+    status: number
+}
+
 const App: React.FC = () => {
+    const [data, loading] = useURLLoader('https://dog.ceo/api/breeds/image/random')
+    const dogResult = data as ShowResult
     return (
         <>
             <div className="App">
                 <header className="App-header">
                     <Hello msg='hello'/>
                     <LinkButton/>
+                    {loading ? <p>读取中</p> : <img alt='dog' src={dogResult?.message}/>}
+
                 </header>
             </div>
         </>
