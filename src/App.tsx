000bf8b5bf5197289a30dfe835f27becba9262de
yangdapeng <yangdapeng@yurun.com>
2020-10-09 10:14:25 +0800

    docs:master分支修改

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import Button from './components/Button'
+import React from "react";
+import Button from "./components/Button";
 
-
+//master分支修改
 const App: React.FC = () => {
-    return (
-        <>
-            <div className="App">
-                app
-                <Button onClick={()=>{
-                    console.log('ddd')
-                }}>ddd</Button>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="App">
+        app
+        <Button
+          onClick={() => {
+            console.log("ddd");
+          }}
+        >
+          ddd
+        </Button>
+      </div>
+    </>
+  );
+};
 export default App;
